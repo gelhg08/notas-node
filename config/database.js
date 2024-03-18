d5dab0f1cafef7f61f239dd35cc06e8d6734bc9c
riwip4-005 <angelicamhg31@gmail.com>
2024-03-18 06:51:05 -0500

    actualizar

--- config/database.js
+++ config/database.js
@@ -1,45 +1,49 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
+let Notas;
 
 const connectDatabase = async () => {
-    try {
-       
-        await mongoose
-        .connect("mongodb+srv://angelicamhg31:QTzqrcasl9rZBTql@cluster0.swtsmnl.mongodb.net/")
-        .then(() => console.log('connect database'))
-        .catch(err => console.log(err))
-
-        await inicializeData()
-
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
-        process.exit(1);
+  try {
+    if (!Notas) {
+      Notas = mongoose.model("Notas", require("../models/userModel").schema);
     }
-}
 
-const inicializeData = async () => { 
-    try {
-        const userNotas = [
-            {
-                nombre: "Juan",
-                apellido: "Pérez",
-                nota1: 70,
-                nota2: 80
-            }, 
-            {
-                nombre: "Angelica",
-                apellido: "Hernandez",
-                nota1: 90,
-                nota2: 80
-            }
-        ]
-        await Notas.insertMany(userNotas);
-        console.log("Users inserted successfully");
+    await mongoose
+      .connect(
+        "mongodb+srv://angelicamhg31:IevENUAk6CTWodsi@mongodb3.wvf0uet.mongodb.net/"
+      )
+      .then(() => console.log("MongoDB connected"))
+      .catch((err) => console.log(err));
 
-    } catch (error) {
-        console.error("Failed to create user:", error);
-        process.exit(1);
-    }
-}
+    await initializeData();
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+    process.exit(1);
+  }
+};
+ 
+const initializeData = async () => {
+  try {
+    const userNotas = [
+      {
+        nombre: "Juan",
+        apellido: "Pérez",
+        nota1: 70,
+        nota2: 80,
+      },
+      {
+        nombre: "Angelica",
+        apellido: "Hernandez",
+        nota1: 90,
+        nota2: 80,
+      },
+    ];
+    await Notas.insertMany(userNotas);
+    console.log("Users inserted successfully");
+  } catch (error) {
+    console.error("Failed to create user:", error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDatabase;
+
